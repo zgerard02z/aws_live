da65fc2c14e0295fe9e1febcd8fb9aaf6799ccb9
zgerard02z <gerardrr-wp22@student.tarc.edu.my>
2025-12-02 11:39:00 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "lowchoonkeat-employee1"
+customregion = "us-east-1"
+
